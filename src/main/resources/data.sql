a3c7fdf5d7ccc03d04171cd6fcefdfdc70e63042
m.groot <m.groot@jandewitenzonen.com>
2024-07-30 10:29:39 +0200

    changed data initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO employee (id, name) VALUES ('klm012345', 'Wilbur Wright');
-INSERT INTO employee (id, name) VALUES ('klm567890', 'Orville Wright');
+MERGE INTO employee (id, name) VALUES ('klm012345', 'Wilbur Wright');
+MERGE INTO employee (id, name) VALUES ('klm567890', 'Orville Wright');
